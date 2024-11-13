562225ef46ae83474186aa95bc162953f66f94eb
EvertonSilvaaDev <ve.tom.19939393@gmail.com>
2024-11-13 01:57:42 -0300

    update script

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ CheckoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 8 && hora < 3;
+    return hora >= 1 && hora < 23;
 }
 
 const spanItem = document.getElementById("date-span")
